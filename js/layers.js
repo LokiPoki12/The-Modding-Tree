2e1448bf61291d1640d9fcf68c989ed0eeae2b85
LokiPoki12 <111561663+LokiPoki12@users.noreply.github.com>
2026-03-01 17:52:19 +1000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -13,11 +13,9 @@ addLayer("p", {
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
+        gainMult() {
         let mult = new Decimal(1)
-
         if (hasUpgrade('p', 13)) mult = mult.times(upgradeEffect('p', 13))
-
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
